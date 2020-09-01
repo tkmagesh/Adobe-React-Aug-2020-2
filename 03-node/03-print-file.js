488d01e0078d6d9af0c88872993e2c48191ac653
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-01 14:32:28 +0530

    file read operation (streams & events)

---
+++ 03-node/03-print-file.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const stream = fs.createReadStream('./sample.txt', { encoding: 'utf8'});
+//readable stream events => open, data, end, close, error
+
+let readCount = 0;
+
+stream.on('data', function(chunk){
+    ++readCount;
+    console.log(chunk);
+});
+
+stream.on('end', function(){
+    console.log('Thats all folks!');
+    console.log('# of chunks read = ', readCount);
+});
+
+stream.on('error', function(err){
+    console.log('Something went wrong');
+    console.log(err);
+});
