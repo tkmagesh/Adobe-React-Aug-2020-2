a6321b9a76dc1b7fa2c8e5d76c3b1b1746be5c01
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-01 17:01:57 +0530

    express api - complete

--- 03-node/my-api-app/routes/bugs.js
+++ 03-node/my-api-app/routes/bugs.js
@@ -21,4 +21,35 @@ router.get('/:id', (req, res, next) => {
     }
 });
 
+router.post('/', (req, res, next) => {
+    const newBugData = req.body,
+        newBugId = bugsList.reduce((result, bug) => result > bug.id ? result : bug.id, 0) + 1,
+        newBug = { ...newBugData, id : newBugId };
+    bugsList.push(newBug);
+    res.status(201).json(newBug);
+});
+
+router.put('/:id', (req, res, next) => {
+    const requestedBugId = parseInt(req.params.id);
+    const updatedBug = req.body;
+    const bug = bugsList.find(bug => bug.id === requestedBugId);
+    if (bug) {
+        bugsList = bugsList.map(b => b.id === requestedBugId ? updatedBug : b);
+        res.json(updatedBug);
+    } else {
+        next()
+    }
+});
+
+router.delete('/:id', (req, res, next) => {
+    const requestedBugId = parseInt(req.params.id);
+    const bug = bugsList.find(bug => bug.id === requestedBugId);
+    if (bug) {
+        bugsList = bugsList.filter(b => b.id !== requestedBugId );
+        res.json({});
+    } else {
+        next()
+    }
+});
+
 module.exports = router;
