1a021d9cb78c43451b4fb502c2310c1391c7ddf4
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-01 15:00:27 +0530

    web server - part - 2

--- 03-node/04-web-server.js
+++ 03-node/04-web-server.js
@@ -1,8 +1,16 @@
 const http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
 const server = http.createServer(function(req /* IncomingMessage */, res /* ServerResponse */){
-    const stream = fs.createReadStream('./index.html');
+    const resourceName = req.url === '/' ? 'index.html' : req.url ;
+    const resourceFullName = path.join(__dirname, resourceName);
+    if (!fs.existsSync(resourceFullName)){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    const stream = fs.createReadStream(resourceFullName);
     stream.on('data', chunk => res.write(chunk));
     stream.on('end', () => res.end());
 });
