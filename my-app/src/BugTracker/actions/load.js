cb75f33080552a4866746bd79d3561268b437a68
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-04 16:28:25 +0530

    introducing the bugApi service

--- my-app/src/BugTracker/actions/load.js
+++ my-app/src/BugTracker/actions/load.js
@@ -1,19 +1,25 @@
-import axios from 'axios';
+/* import axios from 'axios';
 
 function getLocalBugs(){
     return [
         { id : 1, name : 'Local Bug - 1', isClosed : false, createdAt : new Date()},
         { id: 2, name: 'Local Bug - 2', isClosed: false, createdAt: new Date() },
     ];
-}
+} */
 
-function getRemoteBugs() {
+/* function getRemoteBugs() {
     var p = axios.get('http://localhost:3030/bugs');
     var p2 = p.then(function (response) {
         return response.data;
     });
     return p2;
+} */
+
+/* async function getRemoteBugs() {
+    const response = await axios.get('http://localhost:3030/bugs');
+    return response.data;
 }
+ */
 
 /* function load(){
     return function(dispatch){
@@ -25,8 +31,10 @@ function getRemoteBugs() {
     }
 } */
 
+import bugApi from '../services/bugApi';
+
 const load = () => async (dispatch) => {
-    const bugs = await getRemoteBugs();
+    const bugs = await bugApi.getAll();
     const action = { type: 'INIT_BUGS', payload: bugs };
     dispatch(action);
 }
